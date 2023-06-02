42db7f043c10936cadce4798c4e54b14794af029
Joshua Jerred <jdjerred@gmail.com>
2023-06-01 22:31:32 -0500

    number.hpp doc

--- include/BoosterSeat/numbers.hpp
+++ include/BoosterSeat/numbers.hpp
@@ -3,6 +3,12 @@
 
 namespace BoosterSeat {
 
+/**
+ * @brief Rounds a double to two decimal places.
+ * @example 1.2345 -> 1.23
+ * @param d  The double to round.
+ * @return double The rounded double.
+ */
 inline double doubleToPrecisionTwo(double d) {
   int i;
   if (d >= 0)
