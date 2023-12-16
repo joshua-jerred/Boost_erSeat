a09b21c53c849f9645dc2dbd5d9162e6b6bda7af
Joshua Jerred <contact@joshuajer.red>
2023-12-16 03:10:09 -0600

    timer docs

--- include/BoosterSeat/timer.hpp
+++ include/BoosterSeat/timer.hpp
@@ -4,21 +4,49 @@
 #include <BoosterSeat/clock.hpp>
 #include <BoosterSeat/exception.hpp>
 
-namespace BoosterSeat {
+namespace bst {
+
+/// @brief A simple timer class for timing events
 class Timer {
 public:
+  /**
+   * @brief Create a new timer with a timeout
+   * @param timeout_ms The timeout in milliseconds
+   */
   Timer(int timeout_ms);
-  Timer(); // default to 0
-  ~Timer() = default;
 
+  /**
+   * @brief Create a new timer with a timeout of 0. Must call setTimeout before
+   *       using.
+   */
+  Timer();
+
+  /**
+   * @brief Reset the timer to the current time
+   */
   void reset();
+
+  /**
+   * @brief Check if the timer has expired
+   * @return true if the timer has expired, false otherwise
+   */
   bool isDone() const;
+
+  /**
+   * @brief Set the timeout of the timer
+   * @param timeout_ms The timeout in milliseconds
+   */
   void setTimeout(int timeout_ms);
 
+  /**
+   * @brief Set the timer to be done. Changes the start time.
+   */
+  void setDone();
+
 private:
-  clck::TimePoint start_ = clck::Clock::now();
+  BoosterSeat::clck::TimePoint start_ = BoosterSeat::clck::Clock::now();
   int timeout_ms_;
 };
-} // namespace BoosterSeat
+} // namespace bst
 
 #endif /* TIMER_HPP_ */
--- src/timer.cpp
+++ src/timer.cpp
@@ -1,10 +1,10 @@
 #include <BoosterSeat/timer.hpp>
 
-using namespace BoosterSeat;
+using namespace bst;
 
 Timer::Timer(int timeout_ms) : timeout_ms_(timeout_ms) {
   if (timeout_ms < 0) {
-    throw BoosterSeatException("Time must be non-negative");
+    throw BoosterSeat::BoosterSeatException("Time must be non-negative");
   }
 }
 
@@ -12,17 +12,22 @@ Timer::Timer() : timeout_ms_(0) {
 }
 
 void Timer::reset() {
-  start_ = clck::Clock::now();
+  start_ = BoosterSeat::clck::Clock::now();
 }
 
 bool Timer::isDone() const {
-  return (clck::Clock::now() - start_) >=
-         clck::units::Milliseconds(timeout_ms_);
+  return (BoosterSeat::clck::Clock::now() - start_) >=
+         BoosterSeat::clck::units::Milliseconds(timeout_ms_);
 }
 
 void Timer::setTimeout(int timeout_ms) {
   if (timeout_ms < 0) {
-    throw BoosterSeatException("Time must be non-negative");
+    throw BoosterSeat::BoosterSeatException("Time must be non-negative");
   }
   timeout_ms_ = timeout_ms;
+}
+
+void Timer::setDone() {
+  start_ =
+      BoosterSeat::clck::Clock::now() - std::chrono::milliseconds(timeout_ms_);
 }
