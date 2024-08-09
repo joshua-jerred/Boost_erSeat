221064034a67ed75158fdae7b621bef23075a215
Joshua Jerred <contact@joshuajer.red>
2024-08-09 01:25:19 -0500

    de-nest the math namespace

--- include/BoosterSeat/math.hpp
+++ include/BoosterSeat/math.hpp
@@ -25,4 +25,24 @@ inline constexpr double R2D = 180.0 / PI;
 
 } // namespace bst::math
 
+namespace bst {
+/// @brief The mathematical constant pi.
+inline constexpr double PI = 3.14159265358979323846;
+
+/// @brief Half of pi.
+inline constexpr double HALF_PI = PI / 2.0;
+
+/// @brief Three halves of pi.
+inline constexpr double THREE_PI_HALVES = 3.0 * HALF_PI;
+
+/// @brief Two times pi.
+inline constexpr double TWO_PI = 2.0 * PI;
+
+/// @brief Factor to convert degrees to radians.
+inline constexpr double D2R = PI / 180.0;
+
+/// @brief Factor to convert radians to degrees.
+inline constexpr double R2D = 180.0 / PI;
+} // namespace bst
+
 #endif /* BOOSTER_SEAT_MATH_HPP_ */
